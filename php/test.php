63e51277a439571b63fe42edb83486747695eb2b
liuweiwalliam <liuweiwalliam@163.com>
2017-06-28 07:44:37 +0800

    test

--- php/test.php
+++ php/test.php
@@ -1,9 +1,27 @@
-<?php
-echo "Hello Ajax!";
-echo "dfsa"
-?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
 <script>
-    var a = "000-222-333-444";
-    console.log(a.split("_"));
+    class Person
+    {
+        constructor(name,gage)
+        {
+            this.name = name;
+            this.age = age;
+        }
+        ShowInfo() {
+            console.log(this.name,this.age);
+        }
+    };
+    var mike = new('mike',23);
+    console.log(mike);
 </script>
-
+</body>
+</html>
