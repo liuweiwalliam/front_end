cbe31966f29a72e085e77252ccb8f933aa521bcd
liuweiwalliam <liuweiwalliam@163.com>
2017-06-29 10:58:21 +0800

    test

---
+++ webpack/hello.js
@@ -0,0 +1,7 @@
+/**
+ * Created by walliam on 2017/6/28.
+ */
+var num = 'test';
+function hello(str) {
+    console.log(str);
+}
---
+++ webpack/world.js
@@ -0,0 +1,9 @@
+/**
+ * Created by walliam on 2017/6/28.
+ */
+require('./hello.js');
+require('./style.css');
+function world(str) {
+    return
+}
+console.log(num?1:0);
