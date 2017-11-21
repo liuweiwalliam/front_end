6c8acb7edafff0ab08feafb2a54e65f98b43cedd
walliam <liuweiwalliam@163.com>
2017-11-21 15:19:48 +0800

    upd

--- task/webpack.config.js
+++ task/webpack.config.js
@@ -1,16 +1,11 @@
 module.exports = {//注意这里是exports不是export
-    entry:"./webpack/main.jsx",
+    entry: "./webpack/main.jsx",
     output: {
         path: __dirname + "/webpack/build/",//打包后的js文件存放的地方
         filename: "bundle.js"//打包后的js文件名
     },
     module: {
-        loaders:[
-            // {
-            //     test: /\.js[x]?$/,
-            //     exclude: /node_modules/,
-            //     loader: 'babel-loader?presets[]=es2015&presets[]=react'
-            // },
+        loaders: [
             {
                 test: /\.jsx?$/,
                 exclude: /node_modules/,
