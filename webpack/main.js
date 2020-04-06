238ce31f113443eccb01ad773c27dcf97a437e17
walliam <liuweiwalliam@163.com>
2020-04-06 16:40:19 +0800

    删除部分文件

--- webpack/main.js
+++
@@ -1,7 +0,0 @@
-/**
- * Created by walliam on 2017/7/7.
- */
-//入口文件，打包的源文件
-function helloWorld() {
-    console.log('helloWord');
-}
--- webpack/src/main2.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by walliam on 2017/7/7.
- */
-var world = require('./world');
-function helloWorld2() {
-    console.log('helloWord2');
-}
-helloWorld2();
-console.log(world);
